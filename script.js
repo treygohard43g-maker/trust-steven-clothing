61431394cf49b4f82752b26b46b07d31279f585e
treygohard43g-maker <treygohard43g@gmail.com>
2026-08-12 14:06:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
 
+    const menuIcon = document.getElementById("menuIcon");
+    const menuOverlay = document.getElementById("menuOverlay");
+    const closeMenu = document.getElementById("closeMenu");
+
+
+    // OPEN MENU
+
+    menuIcon.addEventListener("click", function () {
+
+        menuOverlay.classList.add("active");
+
+        document.body.style.overflow = "hidden";
+
+    });
+
+
+    // CLOSE MENU
+
+    closeMenu.addEventListener("click", function () {
+
+        menuOverlay.classList.remove("active");
+
+        document.body.style.overflow = "";
+
+    });
+
+
     console.log("Trust & Steven website loaded.");
 
 });
